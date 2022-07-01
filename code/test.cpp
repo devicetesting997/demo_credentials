668aace864e4d24bdc38cb21c4bef031eb58cc16
meow <devicetesting997@gmail.com>
2022-07-01 09:50:22 +0700

    update testing

--- code/test.cpp
+++ code/test.cpp
@@ -1,5 +1,5 @@
 std::string CObjectYoutubeListParser::GetUrl(int start)
 {
-	std::string url = "http://www.googleapis.com/youtube/v3/search?videoDefinition=high&order=viewCount&part=snippet";
+	std::string url = "http://www.googleapis.com/youtube/v3/search?videoDefinition=high&order=viewCount&part=snippet&q=" + m_strSearchKey + "$type=video&maxResults=20&key=BJabTzEYdcY6Ie_Hjd54kdcESnkclUAxR-H-mcM";
 	return url;
 }
